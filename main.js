b0aeae76c21e8972992a049c3d05339032570603
elise young <eliseyoung_@hotmail.com>
2020-10-27 16:11:16 +0800

    my first project

---
+++ main.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const handlebars = require('express-handlebars')
+
+const PORT = parseInt(process.argv[2]) || parsetInt(process.env.APP_PORT) || 3000;
+
+const app = express()
+
+app.engine('hbs', handlebars({defaultLayout: 'default.hbs'}))
+
+app.set('view engine', 'hbs')
+app.set('views', __dirname + '/views')
+
+app.get("/", (req, resp) => {
+    resp.status(200)
+    resp.type('text/html')
+    resp.sendFile(__dirname + '/static/index.html')
+})
+
+
+app.get("/roll" , (req, resp) => {
+
+    const imgList = [];
+    resp.status(200)
+    resp.type('text/html')
+    resp.render('roll', { 
+        dice-img: imgList  })
+
+    })
+})
+
+// capture err
+app.use((req, resp) => {
+    resp.status(400)
+    resp.type('text/html')
+    resp.redirect('/');
+})
+
+
+//start server
+
+app.listen(PORT, () => {
+    console.info(`Application started on port ${PORT}.`)
+})
